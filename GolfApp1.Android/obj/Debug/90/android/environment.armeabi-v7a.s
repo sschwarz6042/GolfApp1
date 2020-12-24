3321e3347b44f9c0d1525b25645f811a91ca3848
sschwarz6042 <71792259+sschwarz6042@users.noreply.github.com>
2020-12-23 19:32:47 -0500

    Changed Scorecard Grid

--- GolfApp1.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ GolfApp1.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"4c0e939d-0f1d-4e36-a10b-0c2c7cc8d1df"
+	.asciz	"3c91bade-3810-496d-b41c-e52ee372aae1"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
